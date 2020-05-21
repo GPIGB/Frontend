839ba9ba14fe1cea74d000bfa0cb80cf12d36e79
jt1174 <jt1174@york.ac.uk>
2020-05-21 15:31:34 +0100

    V1-full view

--- business-view/businessview.js
+++ business-view/businessview.js
@@ -1,14 +1,61 @@
 function showTimeline(){
   document.querySelector('#homescreen').style.display="none";
   document.querySelector('#timegraph').style.display="block";
+  document.querySelector('#qr-codes').style.display="none";
+  document.querySelector('#contact-text').style.display="none";
+  document.querySelector('#certificate-img').style.display="none";
+  document.querySelector('#about-text').style.display="none";
 }
 function goHome(){
   document.querySelector('#homescreen').style.display="inline-block";
   document.querySelector('#timegraph').style.display="none";
+  document.querySelector('#certificate-img').style.display="none";
+  document.querySelector('#qr-codes').style.display="none";
+  document.querySelector('#contact-text').style.display="none";
+  document.querySelector('#about-text').style.display="none";
 }
-
+function showQRcodes(){
+  document.querySelector('#homescreen').style.display="none";
+  document.querySelector('#timegraph').style.display="none";
+  document.querySelector('#qr-codes').style.display="inline-block";
+  document.querySelector('#contact-text').style.display="none";
+  document.querySelector('#certificate-img').style.display="none";
+  document.querySelector('#about-text').style.display="none";
+}
+function contactUs(){
+  document.querySelector('#homescreen').style.display="none";
+  document.querySelector('#timegraph').style.display="none";
+  document.querySelector('#qr-codes').style.display="none";
+  document.querySelector('#contact-text').style.display="inline-block";
+  document.querySelector('#about-text').style.display="none";
+  document.querySelector('#certificate-img').style.display="none";
+}
+function aboutUs(){
+  document.querySelector('#homescreen').style.display="none";
+  document.querySelector('#timegraph').style.display="none";
+  document.querySelector('#qr-codes').style.display="none";
+  document.querySelector('#contact-text').style.display="none";
+  document.querySelector('#about-text').style.display="inline-block";
+  document.querySelector('#certificate-img').style.display="none";
+}
+function showCertificate(){
+  document.querySelector('#homescreen').style.display="none";
+  document.querySelector('#timegraph').style.display="none";
+  document.querySelector('#qr-codes').style.display="none";
+  document.querySelector('#contact-text').style.display="none";
+  document.querySelector('#certificate-img').style.display="block";
+}
+function pritCert() {
+  window.location.href = '/icons/certificate.png';
+}
+document.querySelector('#certificate-img').addEventListener('click',pritCert);
+document.querySelector('#about').addEventListener('click',aboutUs);
+document.querySelector('#certificate').addEventListener('click',showCertificate);
+document.querySelector('#contact').addEventListener('click',contactUs);
+document.querySelector('#timeline').addEventListener('click',showTimeline);
 document.querySelector('#timeline').addEventListener('click',showTimeline);
 document.querySelector('#home').addEventListener('click',goHome);
+document.querySelector('#qr').addEventListener('click',showQRcodes);
 Plotly.newPlot(document.getElementById('timegraph'), [
   { name: 'Paper', y: [1, 2, 4, 8, 16]},
   { name: 'Plastic', y: [1, 3, 7, 10, 11]}],
